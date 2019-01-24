70b06a825074b55e078d8e8aab0873632480b6c8
ChelseaRDinh <chelseadinh94@gmail.com>
2019-01-24 14:03:12 -0800

    Fixed syntax

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 class Square extends React.Component {
 
 	function Square(props) {
-		return (<button className="square" onClick={() 
-				=> this.props.onClick()} >
-				{this.props.value}
+		return (<button className="square" 
+				onClick={props.onClick}>
+				{props.value}
 				</button>
-				);}
+			);
 }
 
 class Board extends React.Component {
